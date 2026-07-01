94c7220a5a525f3c3df997b83ebb22ececad0256
Muhammad Awais Khalil <muhammadawaiskhalil3@gmail.com>
2026-07-01 19:04:57 +0500

    Now i addressed the data extraction part. I learnt it would be much more memory efficient to feed the llm data in chunks as it wouldnt lose context and wont overflow when given a very big document so i scratched previous logiv and worked on this. Whole pdf is extracted into a string and that string is chunked into specific sizes with overlap to preserve context. Chunks obtained wold be fedd to the llm

---
+++ documentChunker.py
@@ -0,0 +1,47 @@
+import PyPDF2
+from typing import List
+
+def chunk_text(text: str, chunk_size: int = 2000, overlap: int = 200)-> List[str]:
+    start = 0
+    end = 0
+    text_length = len(text)
+    chunks = []
+    if text_length <=chunk_size:
+        return [text]
+    
+    #Gets chunks of text while preserving context using a sliding window so that sentences are not cut off without context
+    while start<text_length:
+        end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
+        start += chunk_size - overlap
+
+    return chunks
+
+def get_text_from_pdf(filename: str)-> str:
+    text = ""
+    try:
+        with open(filename, "+rb") as file:
+            reader = PyPDF2.PdfReader(file)
+            for page in reader.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text + '\n'
+            return text
+    except Exception as e:
+        print('Error in reading pdf')
+        return ""
+
+def get_chunks(filename: str, chunk_size: int=2000, overlap: int = 200)-> List[str]:
+    #First get the entire text from pdf
+    text = get_text_from_pdf(filename)
+    if not text.strip():
+        print('No text extracted from PDF')
+        return []
+
+    #Convert text to chunks
+    chunks = get_chunks(text)
+
+    return chunks
+    
+
